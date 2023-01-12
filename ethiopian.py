9048b9153710e32ef800c9e02e051dcde4b67ea4
datadbs <dj9034kr@gmail.com>
2023-01-12 09:45:42 +0900

    feat: Add some code on ethiopian.py

--- ethiopian.py
+++ ethiopian.py
@@ -1,4 +1,17 @@
 def hello():
     print('Good luck !!')
+
+def ethiopian():
+    a= int(input("input multi num1> "))
+    b= int(input("input multi num2> "))
+    sum = 0
+    
+    while(a!=1):
+        a = a//2
+        if a%2 == 1:
+            sum += b*2
+    print(sum)
+
 if __name__ == '__main__':
     hello()
+    ethiopian()
